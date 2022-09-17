a904c011ecd0e4469a014f6c7cc5cb51207452aa
yebbbbiiii <k2932673@naver.com>
2022-09-17 20:47:54 +0900

    Upload Files

---
+++ beginner_300/problem_096.py
@@ -0,0 +1,4 @@
+icecream = {'탱크보이':1200,'폴라포':1200,'빵빠레':1800,'월드콘':1500,'메로나':1000}
+
+icecream1 = list(icecream.values())
+print(icecream1)
---
+++ beginner_300/problem_099.py
@@ -0,0 +1,5 @@
+keys =("apple","pear","peach")
+vals = (300,250,400)
+
+result =dict(zip(keys,vals))
+print(result)
