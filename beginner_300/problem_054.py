ceb10448f79659fc2f3aab59677171bc58a51e16
yebbbbiiii <k2932673@naver.com>
2022-09-17 19:01:27 +0900

    Upload Files

---
+++ beginner_300/problem_054.py
@@ -0,0 +1,3 @@
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨']
+del movie_rank[3]
+print(movie_rank)
---
+++ beginner_300/problem_055.py
@@ -0,0 +1,3 @@
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '배트맨']
+del movie_rank[2:]
+print(movie_rank)
