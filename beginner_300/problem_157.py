f274df9b2feeb52b72dc311cbdd2a9721d6b3fcf
yebbbbiiii <k2932673@naver.com>
2022-09-18 23:48:35 +0900

    Upload Files

---
+++ beginner_300/problem_157.py
@@ -0,0 +1,4 @@
+리스트 = ['dog','cat','parrot']
+for 변수 in 리스트:
+    변수1 = 변수[0].upper() +변수[1:]
+    print(변수1)
---
+++ beginner_300/problem_158.py
@@ -0,0 +1,4 @@
+리스트 = ['hello.py','ex01.py','intro.hwp']
+for 변수 in 리스트:
+    변수1 = 변수.split(".")
+    print(변수1[0])
